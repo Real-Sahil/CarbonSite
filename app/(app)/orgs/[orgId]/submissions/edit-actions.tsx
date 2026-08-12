a1837b85c3c35a8d57239d92143d1c4c77011c44
Sahil <sahilxleo916@gmail.com>
2026-08-12 11:54:01 +0000

    Destructure ocrExtractedData prop in component function

--- app/(app)/orgs/[orgId]/submissions/edit-actions.tsx
+++ app/(app)/orgs/[orgId]/submissions/edit-actions.tsx
@@ -45,6 +45,7 @@ export function SubmissionEditActions({
   orgId,
   submissionId,
   formData,
+  ocrExtractedData,
   emissionCategoryId: initialCategoryId,
   facilityId: initialFacilityId,
   pickupPostcode: initialPickupPostcode,
